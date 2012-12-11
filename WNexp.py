252d7225f0384f58c29bdffd11eaa515787b9999
Xavier Glorot <glx@ghostwheel.(none)>
2012-12-10 19:56:44 -0500

    WordNet experiment script

---
+++ WNexp.py
@@ -0,0 +1,281 @@
+#! /usr/bin/python
+
+from model import *
+
+
+# Utils -------------------------------------------
+def create_random_mat(shape, listidx=None):
+    if listidx is None:
+        listidx = np.arange(shape[0])
+    listidx = listidx[np.random.permutation(len(listidx))]
+    randommat = scipy.sparse.lil_matrix((shape[0], shape[1]),
+            dtype=theano.config.floatX)
+    idx_term = 0
+    for idx_ex in range(shape[1]):
+        if idx_term == len(listidx):
+            idx_term = 0
+        randommat[listidx[idx_term], idx_ex] = 1
+        idx_term += 1
+    return randommat.tocsr()
+
+
+def load_file(path):
+    return scipy.sparse.csr_matrix(cPickle.load(open(path)),
+            dtype=theano.config.floatX)
+
+
+def convert2idx(spmat):
+    rows, cols = spmat.nonzero()
+    return rows[np.argsort(cols)]
+
+class DD(dict):
+    def __getattr__(self, attr):
+        if attr == '__getstate__':
+            return super(DD, self).__getstate__
+        elif attr == '__setstate__':
+            return super(DD, self).__setstate__
+        elif attr == '__slots__':
+            return super(DD, self).__slots__
+        return self[attr]
+
+    def __setattr__(self, attr, value):
+        # Safety check to ensure consistent behavior with __getattr__.
+        assert attr not in ('__getstate__', '__setstate__', '__slots__')
+        self[attr] = value
+
+    def __str__(self):
+        return 'DD%s' % dict(self)
+
+    def __repr__(self):
+        return str(self)
+
+    def __deepcopy__(self, memo):
+        z = DD()
+        for k,kv in self.iteritems():
+            z[k] = copy.deepcopy(kv, memo)
+        return z
+
+# -------------------------------------------------
+
+
+# Experiment function -------------------------------
+def WNexp(state, channel):
+
+    # Show experiment parameters
+    print >> sys.stderr, state
+    np.random.seed(state.seed)
+
+    # Experiment folder
+    if hasattr(channel, 'remote_path'):
+        state.savepath = channel.remote_path + '/'
+    elif hasattr(channel, 'path'):
+        state.savepath = channel.path + '/'
+    else:
+        if not os.path.isdir(state.savepath):
+            os.mkdir(state.savepath)
+
+    # Positives
+    trainl = load_file(state.datapath + state.dataset + '-train-lhs.pkl')
+    trainr = load_file(state.datapath + state.dataset + '-train-rhs.pkl')
+    traino = load_file(state.datapath + state.dataset + '-train-rel.pkl')
+    if state.op == 'SE':
+        traino = traino[-state.Nrel:, :]
+
+    # Negatives
+    trainln = create_random_mat(trainl.shape, np.arange(state.Nsyn))
+    trainrn = create_random_mat(trainr.shape, np.arange(state.Nsyn))
+
+    # Valid set
+    validl = load_file(state.datapath + state.dataset + '-valid-lhs.pkl')
+    validr = load_file(state.datapath + state.dataset + '-valid-rhs.pkl')
+    valido = load_file(state.datapath + state.dataset + '-valid-rel.pkl')
+    if state.op == 'SE':
+        valido = valido[-state.Nrel:, :]
+
+    # Test set
+    testl = load_file(state.datapath + state.dataset + '-test-lhs.pkl')
+    testr = load_file(state.datapath + state.dataset + '-test-rhs.pkl')
+    testo = load_file(state.datapath + state.dataset + '-test-rel.pkl')
+    if state.op == 'SE':
+        testo = testo[-state.Nrel:, :]
+
+    # Index conversion
+    trainlidx = convert2idx(trainl)[:state.neval]
+    trainridx = convert2idx(trainr)[:state.neval]
+    trainoidx = convert2idx(traino)[:state.neval]
+    validlidx = convert2idx(validl)[:state.neval]
+    validridx = convert2idx(validr)[:state.neval]
+    validoidx = convert2idx(valido)[:state.neval]
+    testlidx = convert2idx(testl)[:state.neval]
+    testridx = convert2idx(testr)[:state.neval]
+    testoidx = convert2idx(testo)[:state.neval]
+
+    # Model declaration
+    if not state.loadmodel:
+        # operators
+        if state.op == 'Unstructured':
+            leftop = Unstructured()
+            rightop = Unstructured()
+        elif state.op == 'SME_lin':
+            leftop = LayerLinear(np.random, 'lin', state.ndim, state.ndim,
+                    state.nhid, 'left')
+            rightop = LayerLinear(np.random, 'lin', state.ndim, state.ndim,
+                    state.nhid, 'right')
+        elif state.op == 'SME_bil':
+            leftop = LayerBilinear(np.random, 'lin', state.ndim, state.ndim,
+                    state.nhid, 'left')
+            rightop = LayerBilinear(np.random, 'lin', state.ndim, state.ndim,
+                    state.nhid, 'right')
+        elif state.op == 'SE':
+            leftop = LayerMat('lin', state.ndim, state.nhid)
+            rightop = LayerMat('lin', state.ndim, state.nhid)
+        # embeddings
+        embeddings = Embeddings(np.random, state.Nent, state.ndim, 'embedding')
+        if state.op == 'SE':
+            relationl = Embeddings(np.random, state.Nrel,
+                    state.ndim * state.nhid, 'relationl')
+            relationr = Embeddings(np.random, state.Nrel,
+                    state.ndim * state.nhid, 'relationr')
+            embeddings = [embeddings, relationl, relationr]
+    else:
+        f = open(state.loadmodel)
+        embeddings = cPickle.load(f)
+        leftop = cPickle.load(f)
+        rightop = cPickle.load(f)
+        f.close()
+    simfn = eval(state.simfn + 'sim')
+
+    # Function compilation
+    trainfunc = TrainFn1Member(simfn, embeddings, leftop, rightop,
+            marge=state.marge, rel=False)
+    ranklfunc = RankLeftFnIdx(simfn, embeddings, leftop, rightop,
+            subtensorspec=state.Nsyn)
+    rankrfunc = RankRightFnIdx(simfn, embeddings, leftop, rightop,
+            subtensorspec=state.Nsyn)
+
+    out = []
+    outb = []
+    state.bestvalid = -1
+
+    batchsize = trainl.shape[1] / state.nbatches
+
+    print >> sys.stderr, "BEGIN TRAINING"
+    timeref = time.time()
+    for epoch_count in xrange(1, state.totepochs + 1):
+        # Shuffling
+        order = np.random.permutation(trainl.shape[1])
+        trainl = trainl[:, order]
+        trainr = trainr[:, order]
+        traino = traino[:, order]
+        trainln = trainln[:, np.random.permutation(trainln.shape[1])]
+        trainrn = trainrn[:, np.random.permutation(trainrn.shape[1])]
+
+        for i in range(state.nbatches):
+            tmpl = trainl[:, i * batchsize:(i + 1) * batchsize]
+            tmpr = trainr[:, i * batchsize:(i + 1) * batchsize]
+            tmpo = traino[:, i * batchsize:(i + 1) * batchsize]
+            tmpnl = trainln[:, i * batchsize:(i + 1) * batchsize]
+            tmpnr = trainrn[:, i * batchsize:(i + 1) * batchsize]
+            outtmp = trainfunc(state.lremb, state.lrparam / float(batchsize),
+                    tmpl, tmpr, tmpo, tmpnl, tmpnr)
+            out += [outtmp[0] / float(batchsize)]
+            outb += [outtmp[1]]
+            if type(embeddings) is list:
+                embeddings[0].normalize()
+            else:
+                embeddings.normalize()
+
+        if (epoch_count % state.test_all) == 0:
+            print >> sys.stderr, "-- EPOCH %s (%s seconds per epoch):" % (
+                    epoch_count,
+                    round(time.time() - timeref, 3) / float(state.test_all))
+            timeref = time.time()
+            print >> sys.stderr, "COST >> %s +/- %s, %% updates: %s%%" % (
+                    round(np.mean(out), 4), round(np.std(out), 4),
+                    round(np.mean(outb) * 100, 3))
+            out = []
+            outb = []
+            resvalid = RankingScoreIdx(ranklfunc, rankrfunc,
+                    validlidx, validridx, validoidx)
+            state.valid = np.mean(resvalid[0] + resvalid[1])
+            restrain = RankingScoreIdx(ranklfunc, rankrfunc,
+                    trainlidx, trainridx, trainoidx)
+            state.train = np.mean(restrain[0] + restrain[1])
+            print >> sys.stderr, "\tMEAN RANK >> valid: %s, train: %s" % (
+                    state.valid, state.train)
+            if state.bestvalid == -1 or state.valid < state.bestvalid:
+                restest = RankingScoreIdx(ranklfunc, rankrfunc,
+                        testlidx, testridx, testoidx)
+                state.bestvalid = state.valid
+                state.besttrain = state.train
+                state.besttest = np.mean(restest[0] + restest[1])
+                state.bestepoch = epoch_count
+                # Save model
+                f = open(state.savepath + '/best_valid_model.pkl', 'w')
+                cPickle.dump(embeddings, f, -1)
+                cPickle.dump(leftop, f, -1)
+                cPickle.dump(rightop, f, -1)
+                f.close()
+                print >> sys.stderr, "\t\t##### NEW BEST VALID >> test: %s" % (
+                        state.besttest)
+            # Save model
+            f = open(state.savepath + '/current_model.pkl', 'w')
+            cPickle.dump(embeddings, f, -1)
+            cPickle.dump(leftop, f, -1)
+            cPickle.dump(rightop, f, -1)
+            f.close()
+            state.nbepochs = epoch_count
+            print >> sys.stderr, "\t(the evaluation took %s seconds)" % (
+                round(time.time() - timeref, 3))
+            timeref = time.time()
+            channel.save()
+    return channel.COMPLETE
+
+
+def launch(datapath='data/', dataset='WordNet3.0', Nent=52165,
+        Nsyn=40989, Nrel=35, loadmodel=False, op='Unstructured', simfn='Dot',
+        ndim=50, nhid=50, marge=1., lremb=0.1, lrparam=1., nbatches=100,
+        totepochs=2000, test_all=25, neval=1000, seed=666, savepath='.'):
+
+    # Argument of the experiment script
+    state = DD()
+
+    state.datapath = datapath
+    state.dataset = dataset
+    state.Nent = Nent
+    state.Nsyn = Nsyn
+    state.Nrel = Nrel
+    state.loadmodel = loadmodel
+    state.op = op
+    state.simfn = simfn
+    state.ndim = ndim
+    state.nhid = nhid
+    state.marge = marge
+    state.lremb = lremb
+    state.lrparam = lrparam
+    state.nbatches = nbatches
+    state.totepochs = totepochs
+    state.test_all = test_all
+    state.neval = neval
+    state.seed = seed
+    state.savepath = savepath
+
+    # Jobman channel remplacement
+    class Channel(object):
+        def __init__(self, state):
+            self.state = state
+            f = open(self.state.savepath + '/orig_state.pkl', 'w')
+            cPickle.dump(self.state, f, -1)
+            f.close()
+
+        def save(self):
+            f = open(self.state.savepath + '/current_state.pkl', 'w')
+            cPickle.dump(self.state, f, -1)
+            f.close()
+
+    channel = Channel(state)
+
+    WNexp(state, channel)
+
+if __name__ == '__main__':
+    launch()
