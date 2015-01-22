4be82db87291b6a3dd9168fc0e95991ee4207858
Minh Le <minhle.r7@gmail.com>
2015-01-22 11:18:22 +0100

    add warning message

--- WN/WN_TransE.py
+++ WN/WN_TransE.py
@@ -2,6 +2,11 @@
 from WN_exp import *
 from WN_evaluation import *
 
+if theano.config.floatX == 'float32':
+    sys.stderr.write("""WARNING: Detected floatX=float32 in the configuration.
+This might result in NaN in embeddings after several epochs.
+""")
+
 launch(op='TransE', dataset='WN', simfn='L1', ndim=20, nhid=20, marge=2., lremb=0.01, lrparam=1.,
     nbatches=100, totepochs=1000, test_all=10, neval=1000, savepath='WN_TransE',
     datapath='../data/', Nent=40961,  Nsyn=40943, Nrel=18)
